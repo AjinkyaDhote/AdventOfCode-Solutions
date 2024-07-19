5fdaf904d974a84d77ba27922f134122751a5586
Ajinkya Dhote <ajinkya65@gmail.com>
2024-07-19 16:03:14 -0500

    Continuing Day 12 Part One!

--- AOC_2023/AOC_2023/Day 12.cpp
+++ AOC_2023/AOC_2023/Day 12.cpp
@@ -18,22 +18,128 @@
 
 using namespace std;
 
-int SolvePartOne(string s, int group)
-{
-	
-	size_t position = s.find('?', 0);
-	while (position != s.npos)
-	{
+int SolvePartOne(string record, vector<int> groups);
 
+int CheckValidString(string& s, vector<int>& groups)
+{
+	bool isValid = 0;
+	size_t position = 0;
+	for (int i = 0; i < groups.size(); i++)
+	{
+		//Check if the string satisfies our condition for broken springs.
+		int count = groups[i];
+		//Create a substring
+		string substr = "";
+
+		position = s.find('#', position);
+		if (position != s.npos)
+		{
+			//Check if group is valid	
+			int j = position + 1;
+			while (count > 0 && j != s.npos)
+			{
+				if (s[j] != '#')
+					break;
+				count--;
+			}
+		}
 	}
-		
+
+	return isValid ? 1 : 0;
+}
+
+
+int SolveDot(string record, vector<int> groups)
+{
+	string substr = record.substr(1, record.size());
+	return SolvePartOne(substr, groups);
+}
+
+
+int SolvePound(string record, vector<int> groups, int nextGroup)
+{
+	//Process '#' andd call SolvePartOne
+	//If first character is #, then the first group number is expected to follow i.e if groups[0] = 3, then the sequence should be ###
+	string currentRecord = record.substr(0, nextGroup);
+	replace(currentRecord.begin(), currentRecord.end(), '?', '#'); //replace all '?' to '#'
+
+	if (currentRecord.size() != nextGroup)
+		return 0;
+
+	if (record.size() == nextGroup)
+	{
+		if (groups.size() == 1) //Last Group?
+			return 1;
+		else
+			return 0;
+	}
+
+	if (record[nextGroup] == '?' || record[nextGroup] == '.')
+	{
+		string substr = record.substr(nextGroup + 1, record.size());
+		groups.erase(groups.begin());
+		return SolvePartOne(substr, groups);
+	}
+
+	return 0;
+}
+
+int SolvePartOne(string record, vector<int> groups)
+{
+	//Base case
+	if (groups.size() == 0)
+	{
+		size_t pos = record.find('#');
+		if (pos == record.npos)
+			return 1;
+		else
+			return 0; //More damaged springs we cannot fit 
+	}
+
+	if (record.empty()) //We have groups but we do not have records to check.
+		return 0;
+
+	char nextCharacter = record[0];
+	int nextGroup = groups[0];
+	int ret = 0;
+
+	if (nextCharacter == '#')
+	{
+		ret = SolvePound(record, groups, nextGroup);
+	}
+	else if (nextCharacter == '.')
+	{
+		ret = SolveDot(record, groups);
+	}
+	else if (nextCharacter == '?')
+	{
+		ret =  SolveDot(record, groups) + SolvePound(record, groups, nextGroup);
+	}
+	else
+	{
+		//Invalid
+	}
+	
+
+	//cout << record << " " << "[" << spring.second << "] " << "Possible Arrangements: " << arrangements << "\n";
+
+	return ret;
 }
 
 
 int32_t FindArrangementsForSequence(pair<string, string> spring)
 {
 	string seq = spring.first;
-	string count = spring.second;
+	
+	vector<string> groupsString = Utilities::ReadCommaSeperatedString(spring.second);
+	vector<int> groups;
+
+	for (auto numStr : groupsString)
+	{
+		groups.push_back(stoi(numStr));
+	}
+
+
 	int32_t damaged = 0, operational = 0, unknown = 0;  //damage - '#', operational = '.', unknown = '?'
 
 	for (auto c : seq)
@@ -46,19 +152,8 @@ int32_t FindArrangementsForSequence(pair<string, string> spring)
 			unknown++;
 	}
 
-	//.??..??...?##. 1,1,3
-
-	//? can either be operational(.) or broken(#)
-
-	//4 unknowns
-
+	return SolvePartOne(seq, groups);
 	
-
-
-
-	return -1;
-
-
 }
 
 
@@ -72,14 +167,22 @@ void Solutions::Day12()
 	while (getline(fs, line))
 	{
 		vector<string> split = Utilities::ReadSpaceSeperatedString(line);
-		mp.push_back({split[0], split[1]});
-
-		
+		mp.push_back({split[0], split[1]});	
 	}
+
 
 	for (auto& spring : mp)
 	{
-		int32_t arrangements = FindArrangementsForSequence(spring);
+		vector<string> groupsString = Utilities::ReadCommaSeperatedString(spring.second);
+		vector<int> groups;
+
+		for (auto numStr : groupsString)
+		{
+			groups.push_back(stoi(numStr));
+		}
+		
+		int32_t arrangements = SolvePartOne(spring.first, groups);
+		cout << spring.first << " " << "[" << spring.second << "] " << "Possible Arrangements: " << arrangements << "\n";
 		sum += arrangements;
 	}
 	
