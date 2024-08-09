ad1ab325c7d6ed9c05cafb7db525f4017233512f
Ajinkya Dhote <ajinkya65@gmail.com>
2024-08-09 09:52:20 -0500

    Starting AOC 2015! Language C#

---
+++ AOC_2015/AOC_2015/AOC_2015/Day1/Day1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace AOC_2015
+{
+    public class Day1 : Solutions
+    {
+        public void SolveDay1()
+        {
+            ReadInput();
+        }
+
+        public override void ReadInput()
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "Day1Example.txt");
+            using StreamReader sr = new StreamReader(path);
+            string input = sr.ReadToEnd();
+            ProcessPartOne(input);
+        }
+
+        private static void ProcessPartOne(string input)
+        {
+
+        }
+    }
+}
---
+++ AOC_2015/AOC_2015/AOC_2015/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace AOC_2015
+{
+
+    public abstract class Solutions
+    {
+        public abstract void ReadInput();
+        
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            Day1 day1 = new Day1();
+            day1.SolveDay1();
+        }
+    }
+}
